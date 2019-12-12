8195d65f7792f144e21e83dea4ffd49e1db458f8
angangi <angangi@outlook.com>
2019-12-12 20:07:08 +0800

    无状态组件

--- src/TodoListUI.jsx
+++ src/TodoListUI.jsx
@@ -1,29 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input, Button, List } from 'antd';
-
-class TodoListUI extends Component {
-    
-    render() { 
-        return (  
-            <div style={{margin:'10px'}}>
-                <div>
-                    <Input  placeholder={this.props.inputValue} 
-                            style={{width:'250px', marginRight:'10px'}}
-                            onChange={this.props.changeInputValue}
-                            value={this.props.inputValue}
-                    />
-                    <Button type="primary" onClick={this.props.addItem}>增加</Button>
-                </div>
-                <div style={{margin:'10px', width:'300px'}}>
-                    <List bordered dataSource={this.props.list}
-                        renderItem= { (item,index) => (
-                            <List.Item onClick={(index)=>{this.props.deleteItem(index)}}>{item}</List.Item>
-                        )} 
-                    />
-                </div>
-            </div>
-        );
-    }
-}
  
+// class TodoListUI extends Component { 
+//     render() { 
+        
+//     }
+// }
+
+//无状态组件，不用继承Component
+
+const TodoListUI = (props) => {
+    return ( 
+        <div style={{margin:'10px'}}>
+            <div>
+                <Input  placeholder={props.inputValue} 
+                        style={{width:'250px', marginRight:'10px'}}
+                        onChange={props.changeInputValue}
+                        value={props.inputValue}
+                />
+                <Button type="primary" onClick={props.addItem}>增加</Button>
+            </div>
+            <div style={{margin:'10px', width:'300px'}}>
+                <List bordered dataSource={props.list}
+                    renderItem= { (item,index) => (
+                        <List.Item onClick={()=>
+                            {props.deleteItem(index)}}>{item}
+                        </List.Item>
+                    )} 
+                />
+            </div>
+        </div>
+    );
+}
 export default TodoListUI;
